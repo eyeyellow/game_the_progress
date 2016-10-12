9a0cc30b31beca2397bc9a59395a56291d363235
Frank Aiello <frankvaiello@gmail.com>
2016-10-12 10:57:02 -0700

    Adds fix for spec_helper dependency

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,12 +1,6 @@
-require 'rails_helper'
-
-describe Question do
-  let(:question) { build(:question) }
-  let(:game) { build(:game) }
-
-  context "is_correct" do
-    it "has a value" do
-      expect(question.text).to eq "What is the meaning of life?"
-    end
+FactoryGirl.define do
+  factory :question do
+    text "What is the meaning of life?"
+    association :game
   end
 end
--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative '../spec_helper'
 
 describe Result do
   let(:result) { build(:result) }
